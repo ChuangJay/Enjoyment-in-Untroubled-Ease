0cfd5b77d061dc56fdaf33f6280055ace022f45d
ChuangJay <ChuangJay@outlook.com>
2021-11-17 01:43:15 +0800

    🎉

---
+++ src/main/java/com/example/kunfish/KunfishApplication.java
@@ -0,0 +1,13 @@
+package com.example.kunfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KunfishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KunfishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kunfish/KunfishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kunfish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunfishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
